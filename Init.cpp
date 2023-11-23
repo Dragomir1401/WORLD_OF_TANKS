6b681e867cc607cbb08a10e4443e98843c463e7b
Dragomir Andrei <andreidragomir1401@stud.acs.upb.ro>
2023-11-23 18:17:44 +0200

    added wheel rotation animation

---
+++ Init.cpp
@@ -0,0 +1,316 @@
+#include "lab_m1/tema2/Init.hpp"
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+using namespace m1;
+
+
+/*
+ *  To find out more about `FrameStart`, `Update`, `FrameEnd`
+ *  and the order in which they are called, see `world.cpp`.
+ */
+
+
+InitTema2::InitTema2()
+{
+}
+
+
+InitTema2::~InitTema2()
+{
+}
+
+void InitTema2::CreateTankEntity()
+{
+    const string sourceObjDir = PATH_JOIN(window->props.selfDir, SOURCE_PATH::M1, "tema2", "objects");
+    const string sourceObjDirTank = PATH_JOIN(window->props.selfDir, SOURCE_PATH::M1, "tema2", "objects", "tank", "terog");
+    {
+        Mesh* mesh = new Mesh("body");
+        mesh->LoadMesh(sourceObjDirTank, "tank_txt.obj");
+        tankObjects["body"] = mesh;
+    }
+
+    {
+		Mesh* mesh = new Mesh("tun");
+		mesh->LoadMesh(sourceObjDirTank, "tun.obj");
+		tankObjects["tun"] = mesh;
+	}
+
+    {
+        Mesh* mesh = new Mesh("wheel");
+        mesh->LoadMesh(sourceObjDirTank, "wheel.obj");
+        tankObjects["wheel"] = mesh;
+    }
+
+    // for to 250 load wheel
+    for (int i = 1; i <= 250; i++)
+    {
+		string name = "wheel" + to_string(i);
+		string nameObj = "wheel" + to_string(i) + ".obj";
+		Mesh* mesh = new Mesh(name);
+		mesh->LoadMesh(sourceObjDirTank, nameObj);
+		tankObjects[name] = mesh;
+	}
+ /*
+    {
+		Mesh* mesh = new Mesh("capel");
+		mesh->LoadMesh(sourceObjDir, "capel.obj");
+		tankObjects["capel"] = mesh;
+	}
+
+    {
+		Mesh* mesh = new Mesh("senil");
+		mesh->LoadMesh(sourceObjDir, "senil.obj");
+		tankObjects["senil"] = mesh;
+	}*/
+}
+
+void InitTema2::ParseTextures()
+{
+    const string sourceTextureDir = PATH_JOIN(window->props.selfDir, SOURCE_PATH::M1, "tema2", "textures");
+
+    // Create a texture object for tank_green
+    Texture2D* texture = new Texture2D();
+    texture->Load2D(PATH_JOIN(sourceTextureDir, "tank_green.png").c_str(), GL_REPEAT);
+    textures["tank_green"] = texture;
+}
+
+void InitTema2::RenderMeshTexture(Mesh* mesh, Shader* shader, const glm::mat4& modelMatrix, Texture2D* texture1, Texture2D* texture2)
+{
+    if (!mesh || !shader || !shader->program)
+        return;
+
+    // Render an object using the specified shader and the specified position
+    shader->Use();
+    glUniformMatrix4fv(shader->loc_view_matrix, 1, GL_FALSE, glm::value_ptr(GetSceneCamera()->GetViewMatrix()));
+    glUniformMatrix4fv(shader->loc_projection_matrix, 1, GL_FALSE, glm::value_ptr(GetSceneCamera()->GetProjectionMatrix()));
+    glUniformMatrix4fv(shader->loc_model_matrix, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+
+    //// Set any other shader uniforms that you need
+    int loc_texture_1 = glGetUniformLocation(shader->program, "texture_1");
+    int loc_texture_2 = glGetUniformLocation(shader->program, "texture_2");
+
+    if (texture1)
+    {
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1->GetTextureID());
+        glUniform1i(loc_texture_1, 0);
+    }
+
+    if (texture2)
+    {
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2->GetTextureID());
+        glUniform1i(loc_texture_2, 1);
+    }
+
+    mesh->Render();
+}
+
+void InitTema2::RenderTexturedMesh(Mesh* mesh, Shader* shader, const glm::mat4& modelMatrix, Texture2D* texture1, Texture2D* texture2)
+{
+    if (!mesh || !shader || !shader->GetProgramID())
+        return;
+
+    // Render an object using the specified shader and the specified position
+    glUseProgram(shader->program);
+
+    // Bind model matrix
+    GLint loc_model_matrix = glGetUniformLocation(shader->program, "Model");
+    glUniformMatrix4fv(loc_model_matrix, 1, GL_FALSE, glm::value_ptr(modelMatrix));
+
+    // Bind view matrix
+    glm::mat4 viewMatrix = GetSceneCamera()->GetViewMatrix();
+    int loc_view_matrix = glGetUniformLocation(shader->program, "View");
+    glUniformMatrix4fv(loc_view_matrix, 1, GL_FALSE, glm::value_ptr(viewMatrix));
+
+    // Bind projection matrix
+    glm::mat4 projectionMatrix = GetSceneCamera()->GetProjectionMatrix();
+    int loc_projection_matrix = glGetUniformLocation(shader->program, "Projection");
+    glUniformMatrix4fv(loc_projection_matrix, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+
+    // Set any other shader uniforms that you need
+    int loc_texture_1 = glGetUniformLocation(shader->program, "texture_1");
+    int loc_texture_2 = glGetUniformLocation(shader->program, "texture_2");
+
+    if (texture1)
+    {
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1->GetTextureID());
+        glUniform1i(loc_texture_1, 0);
+    }
+
+    if (texture2)
+    {
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2->GetTextureID());
+        glUniform1i(loc_texture_2, 1);
+    }
+
+    // Draw the object
+    glBindVertexArray(mesh->GetBuffers()->m_VAO);
+    glDrawElements(mesh->GetDrawMode(), static_cast<int>(mesh->indices.size()), GL_UNSIGNED_INT, 0);
+}
+
+void m1::InitTema2::RenderTankEntity()
+{
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, glm::vec3(-1.0f, -1.1f, 1.2f));
+        RenderMesh(tankObjects["body"], shaders["ShaderTank"], modelMatrix);
+    }
+
+   {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, glm::vec3(10.0f, 2.25f, 0));
+        RenderMesh(tankObjects["tun"], shaders["ShaderTank"], modelMatrix);
+    }
+
+   {
+       glm::mat4 modelMatrix = glm::mat4(1);
+       modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 7.0f, 0));
+       RenderMesh(tankObjects["wheel"], shaders["ShaderTank"], modelMatrix);
+   }
+
+   {
+       // Rend the wheel at animationIndex
+       string name = "wheel" + to_string(animationIndex);
+       glm::mat4 modelMatrix = glm::mat4(1);
+       modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 9.0f, 0));
+       // If animation index is = 1 then translate the wheel to the last position
+       if (animationIndex == 1)
+       {
+		   modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 0, 0));
+	   }
+       RenderMesh(tankObjects[name], shaders["ShaderTank"], modelMatrix);
+   }
+   /*
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, glm::vec3(0.5f, -0.2f, 0));
+        RenderTexturedMesh(tankObjects["capel"], shaders["ShaderTank"], modelMatrix);
+    }
+
+    {
+        glm::mat4 modelMatrix = glm::mat4(1);
+        modelMatrix = glm::translate(modelMatrix, glm::vec3(0, 0, 0));
+        RenderMeshTexture(tankObjects["senil"], shaders["ShaderTank"], modelMatrix);
+    }*/
+}
+
+void InitTema2::Init()
+{
+    CreateTankEntity();
+    ParseTextures();
+
+    // Create a shader program for drawing face polygon with the color of the normal
+    {
+        Shader *shader = new Shader("ShaderTank");
+        shader->AddShader(PATH_JOIN(window->props.selfDir, SOURCE_PATH::M1, "tema2", "shaders", "TextureVertexShader.glsl"), GL_VERTEX_SHADER);
+        shader->AddShader(PATH_JOIN(window->props.selfDir, SOURCE_PATH::M1, "tema2", "shaders", "FragTextureShader.glsl"), GL_FRAGMENT_SHADER);
+        shader->CreateAndLink();
+        shaders[shader->GetName()] = shader;
+    }
+}
+
+void InitTema2::FrameStart()
+{
+    // Clears the color buffer (using the previously set color) and depth buffer
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glm::ivec2 resolution = window->GetResolution();
+    // Sets the screen area where to draw
+    glViewport(0, 0, resolution.x, resolution.y);
+}
+
+
+void InitTema2::Update(float deltaTimeSeconds)
+{
+    RenderTankEntity();
+
+    // If we press W, increase the animation index
+    if (window->KeyHold(GLFW_KEY_W))
+    {
+		animationIndex++;
+        if (animationIndex > 250)
+        {
+			animationIndex = 1;
+		}
+	}
+
+    // If we press S, decrease the animation index
+    if (window->KeyHold(GLFW_KEY_S))
+    {
+        animationIndex--;
+        if (animationIndex < 1)
+        {
+			animationIndex = 250;
+		}
+    }
+
+
+    //elapsedTime = Engine::GetElapsedTime();
+}
+
+
+void InitTema2::FrameEnd()
+{
+    DrawCoordinateSystem();
+}
+
+
+/*
+ *  These are callback functions. To find more about callbacks and
+ *  how they behave, see `input_controller.h`.
+ */
+
+
+void InitTema2::OnInputUpdate(float deltaTime, int mods)
+{
+    // Add key press event
+}
+
+
+void InitTema2::OnKeyPress(int key, int mods)
+{
+    // On "W" press increase the animation index
+   
+}
+
+
+void InitTema2::OnKeyRelease(int key, int mods)
+{
+    // Add key release event
+}
+
+
+void InitTema2::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
+{
+    // Add mouse move event
+}
+
+
+void InitTema2::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
+{
+    // Add mouse button press event
+}
+
+
+void InitTema2::OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods)
+{
+    // Add mouse button release event
+}
+
+
+void InitTema2::OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY)
+{
+}
+
+
+void InitTema2::OnWindowResize(int width, int height)
+{
+}
---
+++ Init.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector>
+
+#include "components/simple_scene.h"
+
+
+namespace m1
+{
+    class InitTema2 : public gfxc::SimpleScene
+    {
+     public:
+        InitTema2();
+        ~InitTema2();
+
+        void Init() override;
+
+     private:
+        void CreateTankEntity();
+        void RenderTexturedMesh(Mesh* mesh, Shader* shader, const glm::mat4& modelMatrix, Texture2D* texture1 = NULL, Texture2D* texture2 = NULL);
+        void ParseTextures();
+        void RenderMeshTexture(Mesh* mesh, Shader* shader, const glm::mat4& modelMatrix, Texture2D* texture1 = NULL, Texture2D* texture2 = NULL);
+        void RenderTankEntity();
+        void FrameStart() override;
+        void Update(float deltaTimeSeconds) override;
+        void FrameEnd() override;
+
+        void OnInputUpdate(float deltaTime, int mods) override;
+        void OnKeyPress(int key, int mods) override;
+        void OnKeyRelease(int key, int mods) override;
+        void OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY) override;
+        void OnMouseBtnPress(int mouseX, int mouseY, int button, int mods) override;
+        void OnMouseBtnRelease(int mouseX, int mouseY, int button, int mods) override;
+        void OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY) override;
+        void OnWindowResize(int width, int height) override;
+
+        float elapsedTime;
+        std::unordered_map<std::string, Mesh*> tankObjects;
+        std::unordered_map<std::string, Texture2D*> textures;
+        int animationIndex = 1;
+        glm::vec3 lastWheelPosition;
+    };
+}   // namespace m1
